f8f0491fc85124bc9ce802e455bbd89c279f48d9
Jaydip Patel <jaydippatel@Jaydips-MacBook-Air.local>
2022-05-20 10:57:18 +0530

    initial commit

---
+++ src/network/Network.js
@@ -0,0 +1,12 @@
+const BOBA = {
+    chainId: "0x28",
+    chainName: "Rinkeby BOBA",
+    rpcUrls: ["https://rinkeby.boba.network"],
+    nativeCurrency: {
+        name: "Rinkeby",
+        symbol: "ETH",
+        decimals: 18,
+    },
+    blockExplorerUrls: ["https://blockexplorer.rinkeby.boba.network"],
+} 
+export { BOBA };
